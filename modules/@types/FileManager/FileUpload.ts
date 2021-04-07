6d388ea961e1cc8c4c0e4270666c93abe55d2c12
purpureous <purpureous@naver.com>
2021-04-07 17:47:28 +0900

    TSConfig Changed

--- modules/@types/FileManager/FileUpload.ts
+++
@@ -1,9 +0,0 @@
-import * as multer from 'multer'
-import * as aws from 'aws-sdk'
-
-
-export abstract class UploaderInterface{
-   private s3: aws.S3
-   private storage: multer.StorageEngine
-   public uploadFile(src: string): multer.Multer { return multer.default({})}
-}
--- routes/@types/evalCampaign.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express'
-var router = express.Router()
-
-const rate = require('./campaignRate')
-
-router.use('/rate', rate)
-
-module.exports = router
