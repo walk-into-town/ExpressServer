438869320c35c18dd89aecc3416c84c80d1e64a0
purpureous <purpureous@naver.com>
2021-04-07 17:49:00 +0900

    Express App

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const routes = require('./routes/@js')
+const routes = require('./@js/routes')
 
 app.use(routes)
 
