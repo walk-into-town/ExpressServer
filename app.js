eeedd9d15a896f6518ce157e6e287eba78b64c81
purpureous <purpureous@naver.com>
2021-04-17 10:22:58 +0900

    Express App

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var app = express();
+const middle = require('./@js/modules/sessionMiddle')
 
+var app = express();
 
 /**
  * set static img folder
@@ -37,7 +38,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 /**
  * Set session
  */
@@ -55,20 +55,26 @@ const dynamodb = new AWS.DynamoDB()
 var DynamoDBStore = require('connect-dynamodb')(session)
 app.use(session({
   secret: 'SessionSecret',
-  resave: false,                //세션에 변경사항 생기기면 저장 O
-  saveUninitialized: true,      //세션 생성 전에는 저장 X
+  resave: false,                //세션에 변경사항이 생길때만 저장 O
+  saveUninitialized: false,     //세션 생성 전에는 저장 X
+  rolling: true,                //요청때마다 세션 갱신
   store: new DynamoDBStore({    //default: 메모리에 저장 -> 분산 처리 문제 + EC2의 메모리 부족 -> DynamoDB에 저장
     table: 'Session',           //세션을 저장할 테이블 명
-    reapInterval: 60*1000*60,   //만료된 세션 정리 간격 (ms). 1시간마다 정리
+    reapInterval: 1000*60*60,   //만료된 세션 정리 간격 (ms). 1시간마다 정리
     client: dynamodb            //사용할 DyanmoDB 클라이언트.
   }),
   cookie:{
-    maxAge: 1000*60*60*3,           //토큰 유효 시간 (ms). 3시간동안 유효
+    maxAge: 1000*60*3,           //토큰 유효 시간 (ms). 3시간동안 유효
     httpOnly: true,              //http통신에서만 쿠키 확인 가능 -> 클라이언트의 script에서 불가
 //    secure: true               //https에서만 쿠키 전달
   }
 }))
 
+/**
+ * Persional MiddleWares
+ */
+
+ app.use(middle.sessionCheck)
 
 /**
  * set routes
