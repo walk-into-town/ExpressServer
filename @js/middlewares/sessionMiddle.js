254fdc491c75cfe028157919ff93b9c9c36f631e
purpureous <purpureous@naver.com>
2021-04-27 00:06:31 +0900

    Middleware that controls Session

---
+++ @js/middlewares/sessionMiddle.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sessionCheck = void 0;
+const SessionManager_1 = __importDefault(require("../modules/DBManager/SessionManager"));
+const sessionCheck = function (req, res, next) {
+    if (req.originalUrl == '/member/login') { //로그인 요청시
+        let params = req.body;
+        if (req.session.user == undefined) { //세션 없이 로그인 시 기존에 로그인된 세션 삭제
+            let sessman = new SessionManager_1.default(req, res);
+            sessman.findByUId(params.id).then(function () {
+                sessman.deleteSession(res.locals.result);
+            }.bind(this));
+            next();
+            return;
+        }
+        if (req.session.user.id != undefined) { //세션의 id가 존재 = 이미 로그인함
+            let result = {
+                result: 'failed',
+                error: 'Already logged in'
+            };
+            res.status(400).send(result);
+            return;
+        }
+        next();
+        return;
+    }
+    if (req.session == undefined) { //로그인 이외 요청시
+        let result = {
+            result: 'failed',
+            error: 'Session Expired'
+        };
+        res.status(200).send();
+        return;
+    }
+    next();
+};
+exports.sessionCheck = sessionCheck;
