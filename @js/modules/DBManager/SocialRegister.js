46c7bbec3a2ffbd7d5dc3aa779d4eff9da1d340d
purpureous <purpureous@naver.com>
2021-04-28 17:33:35 +0900

    social login/member

---
+++ @js/modules/DBManager/SocialRegister.js
@@ -0,0 +1,61 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const DBConnection_1 = __importDefault(require("./DBConnection"));
+const bcrypt = __importStar(require("bcrypt"));
+class SocialRegister {
+    insert(params) {
+        let dbConn = new DBConnection_1.default();
+        let dynamodb = dbConn.getDynamoDB();
+        let pw;
+        let saltRounds = 10;
+        const run = () => __awaiter(this, void 0, void 0, function* () {
+            yield bcrypt.hash(params.pw, saltRounds).then(function (hash) {
+                pw = hash;
+            });
+            var queryParams = {
+                TableName: 'Member',
+                Item: {
+                    id: params.id,
+                    pw: pw,
+                    nickname: params.nickname,
+                    isManager: params.isManager
+                },
+                ConditionExpression: "attribute_not_exists(id)" //항목 추가하기 전에 이미 존재하는 항목이 있을 경우 pk가 있을 때 조건 실패. pk는 반드시 있어야 하므로 replace를 방지
+            };
+            yield dynamodb.put(queryParams).promise();
+        });
+        return run();
+    }
+}
+exports.default = SocialRegister;
---
+++ @types/modules/DBManager/SocialRegister.ts
@@ -0,0 +1,27 @@
+import DBConnection from './DBConnection'
+import * as bcrypt from 'bcrypt'
+
+export default class SocialRegister{
+    public insert(params: any) {
+        let dbConn = new DBConnection()
+        let dynamodb = dbConn.getDynamoDB()
+        let pw: string; let saltRounds = 10
+        const run = async () => {
+            await bcrypt.hash(params.pw, saltRounds).then(function(hash){
+                pw = hash
+              })
+              var queryParams = {
+                TableName: 'Member',
+                Item: {
+                    id: params.id,
+                    pw: pw,
+                    nickname: params.nickname,
+                    isManager: params.isManager
+                },
+                ConditionExpression: "attribute_not_exists(id)"      //항목 추가하기 전에 이미 존재하는 항목이 있을 경우 pk가 있을 때 조건 실패. pk는 반드시 있어야 하므로 replace를 방지
+            }
+            await dynamodb.put(queryParams).promise()
+        }
+        return run()
+    }
+}
