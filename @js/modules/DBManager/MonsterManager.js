d82629088ea3826b8f62af0b11d520a52d687e6e
purpureous <purpureous@naver.com>
2021-04-29 22:35:36 +0900

    Monster DB

--- @js/modules/DBManager/MonsterManager.js
+++ @js/modules/DBManager/MonsterManager.js
@@ -1,8 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const FeatureManager_1 = require("./FeatureManager");
 class MonsterManager extends FeatureManager_1.FeatureManager {
     insert(params) {
+        params.number = parseInt(params.number);
+        var queryParams = {
+            TableName: 'Monster',
+            Key: { 'number': 100 },
+            UpdateExpression: 'set imgs = list_append(imgs, :newimgs)',
+            ExpressionAttributeValues: { ':newimgs': params.imgs },
+            ExpressionAttributeNames: { '#number': 'number' },
+            ReturnValues: 'UPDATED_NEW',
+            ConditionExpression: "attribute_exists(#number)"
+        };
+        const run = () => __awaiter(this, void 0, void 0, function* () {
+            let data = yield this.Dynamodb.update(queryParams).promise();
+            let result = {
+                result: 'success',
+                message: data.Attributes.imgs
+            };
+            this.res.status(200).send(result);
+        });
+        try {
+            run();
+        }
+        catch (err) {
+            this.res.status(400).send('DB Error. Please Connect Manager');
+        }
     }
     read(params) {
         throw new Error("Method not implemented.");
--- @types/modules/DBManager/MonsterManager.ts
+++ @types/modules/DBManager/MonsterManager.ts
@@ -2,8 +2,31 @@ import { FeatureManager } from "./FeatureManager";
 import {Monster} from '../../models/Monster'
 
 export default class MonsterManager extends FeatureManager{
-    public insert(params: Monster): void {
-        
+    public insert(params: any): void {
+        params.number = parseInt(params.number)
+        var queryParams = {
+            TableName: 'Monster',
+            Key: {'number': 100},
+            UpdateExpression: 'set imgs = list_append(imgs, :newimgs)',
+            ExpressionAttributeValues: {':newimgs': params.imgs},
+            ExpressionAttributeNames: {'#number' : 'number'},
+            ReturnValues: 'UPDATED_NEW',
+            ConditionExpression: "attribute_exists(#number)"
+        }
+        const run = async() => {
+            let data = await this.Dynamodb.update(queryParams).promise()
+            let result = {
+                result: 'success',
+                message: data.Attributes.imgs
+            }
+            this.res.status(200).send(result)
+        }
+        try{
+            run()
+        }
+        catch(err){
+            this.res.status(400).send('DB Error. Please Connect Manager')
+        }
     }
     public read(params: any): void {
         throw new Error("Method not implemented.");
