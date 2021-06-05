3d3276d9c6c2047c15952ae9c3b32f600ada85fc
purpureous <purpureous@naver.com>
2021-06-05 21:47:53 +0900

    fix not to login

--- @types/routes/manager/manager.ts
+++ @types/routes/manager/manager.ts
@@ -7,25 +7,25 @@ import Reportmanager from '../../modules/DBManager/ReportManager'
 
 var router = express.Router()
 
-router.post('/report', isAuthenticated, function(req: express.Request, res: express.Response){
+router.post('/report', function(req: express.Request, res: express.Response){
     let reportDB = new Reportmanager(req, res)
     let query = req.body
     reportDB.insert(query)
 })
 
-router.get('/report', isAuthenticated, function(req: express.Request, res: express.Response){
+router.get('/report', function(req: express.Request, res: express.Response){
     let reportDB = new Reportmanager(req, res)
     let query = req.query
     reportDB.read(query)
 })
 
-router.put('/report', isAuthenticated, function(req: express.Request, res: express.Response){
+router.put('/report', function(req: express.Request, res: express.Response){
     let reportDB = new Reportmanager(req, res)
     let query = req.body
     reportDB.update(query)
 })
 
-router.put('/report/process', isAuthenticated, function(req: express.Request, res: express.Response){
+router.put('/report/process', function(req: express.Request, res: express.Response){
     
 })
 
