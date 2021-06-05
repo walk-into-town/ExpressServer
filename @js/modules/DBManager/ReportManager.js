26e3888730b826662e99e1c9ce8e398c280e87ea
purpureous <purpureous@naver.com>
2021-06-05 14:56:06 +0900

    add report insert

---
+++ @js/modules/DBManager/ReportManager.js
@@ -0,0 +1,163 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const result_1 = require("../../static/result");
+const CryptoJS = __importStar(require("crypto-js"));
+const FeatureManager_1 = require("./FeatureManager");
+class Reportmanager extends FeatureManager_1.FeatureManager {
+    insert(params) {
+        let hash = CryptoJS.SHA256(Date().toString() + params.targetId + Math.random());
+        let id = hash.toString(CryptoJS.enc.Base64);
+        let targetId = params.targetId;
+        let userId = this.req.session.passport.user.id;
+        let targetUser = params.targetUser;
+        let description = params.description;
+        let type = params.type;
+        let typeId = params.typeId;
+        let processed = false;
+        let date = new Date(Date.now() + 9 * 60 * 60 * 1000).toISOString();
+        let getParam = {
+            TableName: params.type,
+            KeyConditionExpression: 'id = :id',
+            ExpressionAttributeValues: { ':id': typeId },
+            ProjectionExpression: 'comments'
+        };
+        let memberParam = {
+            TableName: 'Member',
+            KeyConditionExpression: 'id = :id',
+            ExpressionAttributeValues: { ':id': targetUser }
+        };
+        let reportParam = {
+            TableName: 'Report'
+        };
+        let insertParams = {
+            TableName: 'Report',
+            Item: {
+                id: id,
+                targetId: targetId,
+                description: description,
+                userId: userId,
+                targetUser: targetUser,
+                date: date,
+                type: type,
+                processed: processed
+            },
+            ConditionExpression: "attribute_not_exists(id)" //항목 추가하기 전에 이미 존재하는 항목이 있을 경우 pk가 있을 때 조건 실패. pk는 반드시 있어야 하므로 replace를 방지
+        };
+        const run = () => __awaiter(this, void 0, void 0, function* () {
+            try {
+                console.log('요청 유효성 검사중');
+                console.log('type 유효성 검사중');
+                if (type != 'Campaign' && type != 'Pinpoint') { // type이 잘못된 경우
+                    console.log('잘못된 type');
+                    result_1.fail.error = result_1.error.typeMiss;
+                    result_1.fail.errdesc = 'type은 Campaign || Pinpoint 중 하나여야 합니다.';
+                    this.res.status(400).send(result_1.fail);
+                    return;
+                }
+                console.log('type 유효성 검사 통과');
+                console.log('typeId 유효성 검사중');
+                let result = yield this.Dynamodb.query(getParam).promise();
+                if (result.Items[0] == undefined) {
+                    console.log('typeid를 찾을 수 없습니다.');
+                    result_1.fail.error = result_1.error.dataNotFound;
+                    result_1.fail.errdesc = 'typeid를 찾을 수 없습니다.';
+                    this.res.status(400).send(result_1.fail);
+                    return;
+                }
+                console.log('targetUser 유효성 검사중');
+                let memberResult = yield this.Dynamodb.query(memberParam).promise();
+                if (memberResult.Items[0] == undefined) {
+                    result_1.fail.error = result_1.error.dataNotFound;
+                    result_1.fail.errdesc = 'targetUser를 찾을 수 없습니다.';
+                    this.res.status(400).send(result_1.fail);
+                    return;
+                }
+                console.log('targetUser 유효성 검사 통과');
+                let comments = result.Items[0].comments;
+                console.log('typeId 유효성 검사 통과');
+                console.log('targetId 유효성 검사중');
+                for (let i = 0; i < comments.length; i++) {
+                    if (comments[i].id == targetId) {
+                        if (comments[i].userId != targetUser) {
+                            console.log('targetUser가 잘못되었습니다.');
+                            result_1.fail.error = result_1.error.invalReq;
+                            result_1.fail.errdesc = 'targetUser가 잘못되었습니다.';
+                            this.res.status(400).send(result_1.fail);
+                            return;
+                        }
+                        console.log('targetId 유효함');
+                        break;
+                    }
+                    if (i == comments.length - 1) {
+                        console.log('targetId를 찾을 수 없습니다.');
+                        result_1.fail.error = result_1.error.dataNotFound;
+                        result_1.fail.errdesc = 'targetId를 찾을 수 없습니다.';
+                        this.res.status(400).send(result_1.fail);
+                        return;
+                    }
+                }
+                console.log('targetId 요효성 검사 통과');
+                console.log('중복 신고 여부 확인중');
+                let reportResult = yield this.Dynamodb.scan(reportParam).promise();
+                let reports = reportResult.Items;
+                for (const report of reports) {
+                    if (report.targetId == targetId && report.targetUser == targetUser && report.userId == userId) {
+                        result_1.fail.error = result_1.error.invalReq;
+                        result_1.fail.errdesc = '이미 신고하신 댓글입니다.';
+                        this.res.status(400).send(result_1.fail);
+                        return;
+                    }
+                }
+                console.log('신고 등록 시작');
+                yield this.Dynamodb.put(insertParams).promise();
+                console.log('신고 등록 성공');
+                result_1.success.data = '신고 등록 성공';
+                this.res.status(201).send(result_1.success);
+            }
+            catch (err) {
+                result_1.fail.error = result_1.error.dbError;
+                result_1.fail.errdesc = err;
+                this.res.status(521).send(result_1.fail);
+            }
+        });
+        run();
+    }
+    read(params, ReadType) {
+        throw new Error("Method not implemented.");
+    }
+    update(params) {
+        throw new Error("Method not implemented.");
+    }
+    delete(params) {
+        throw new Error("Method not implemented.");
+    }
+}
+exports.default = Reportmanager;
---
+++ @types/modules/DBManager/ReportManager.ts
@@ -0,0 +1,135 @@
+import { error, fail, success } from "../../static/result";
+import * as CryptoJS from 'crypto-js'
+import { FeatureManager, toRead } from "./FeatureManager";
+
+export default class Reportmanager extends FeatureManager{
+    public insert(params: any): void {
+        let hash = CryptoJS.SHA256(Date().toString() + params.targetId + Math.random())
+        let id: string = hash.toString(CryptoJS.enc.Base64)
+        let targetId: string = params.targetId
+        let userId: string = this.req.session.passport.user.id
+        let targetUser: string = params.targetUser
+        let description: string = params.description
+        let type: string = params.type
+        let typeId: string = params.typeId
+        let processed: boolean = false
+        let date = new Date(Date.now() + 9 * 60 * 60 * 1000).toISOString()
+        
+        let getParam = {
+            TableName: params.type,
+            KeyConditionExpression: 'id = :id',
+            ExpressionAttributeValues: {':id': typeId},
+            ProjectionExpression: 'comments'
+        }
+        let memberParam = {
+            TableName: 'Member',
+            KeyConditionExpression: 'id = :id',
+            ExpressionAttributeValues: {':id': targetUser}
+        }
+        let reportParam = {
+            TableName: 'Report'
+        }
+        let insertParams = {
+            TableName: 'Report',
+            Item: {
+                id: id,
+                targetId: targetId,
+                description: description,
+                userId: userId,
+                targetUser: targetUser,
+                date: date,
+                type: type,
+                processed: processed
+            },
+            ConditionExpression: "attribute_not_exists(id)"      //항목 추가하기 전에 이미 존재하는 항목이 있을 경우 pk가 있을 때 조건 실패. pk는 반드시 있어야 하므로 replace를 방지
+        }
+        const run = async () => {
+            try{
+                console.log('요청 유효성 검사중')
+                console.log('type 유효성 검사중')
+                if(type != 'Campaign' && type != 'Pinpoint'){       // type이 잘못된 경우
+                    console.log('잘못된 type')
+                    fail.error = error.typeMiss
+                    fail.errdesc = 'type은 Campaign || Pinpoint 중 하나여야 합니다.'
+                    this.res.status(400).send(fail)
+                    return;
+                }
+                console.log('type 유효성 검사 통과')
+                console.log('typeId 유효성 검사중')
+                let result = await this.Dynamodb.query(getParam).promise()
+                if(result.Items[0] == undefined){
+                    console.log('typeid를 찾을 수 없습니다.');
+                    fail.error = error.dataNotFound
+                    fail.errdesc = 'typeid를 찾을 수 없습니다.'
+                    this.res.status(400).send(fail)
+                    return;
+                }
+                console.log('targetUser 유효성 검사중')
+                let memberResult = await this.Dynamodb.query(memberParam).promise()
+                if(memberResult.Items[0] == undefined){
+                    fail.error = error.dataNotFound
+                    fail.errdesc = 'targetUser를 찾을 수 없습니다.'
+                    this.res.status(400).send(fail)
+                    return;
+                }
+                console.log('targetUser 유효성 검사 통과')
+                let comments: Array<any> = result.Items[0].comments
+                console.log('typeId 유효성 검사 통과')
+                console.log('targetId 유효성 검사중')
+                for(let i =0; i < comments.length; i++){
+                    if(comments[i].id == targetId){
+                        if(comments[i].userId != targetUser){
+                            console.log('targetUser가 잘못되었습니다.')
+                            fail.error = error.invalReq
+                            fail.errdesc = 'targetUser가 잘못되었습니다.'
+                            this.res.status(400).send(fail)
+                            return
+                        }
+                        console.log('targetId 유효함')
+                        break;
+                    }
+                    if(i == comments.length - 1){
+                        console.log('targetId를 찾을 수 없습니다.')
+                        fail.error = error.dataNotFound
+                        fail.errdesc = 'targetId를 찾을 수 없습니다.'
+                        this.res.status(400).send(fail)
+                        return;
+                    }
+                }
+                console.log('targetId 요효성 검사 통과')
+                console.log('중복 신고 여부 확인중')
+                let reportResult = await this.Dynamodb.scan(reportParam).promise()
+                let reports = reportResult.Items
+                for(const report of reports){
+                    if(report.targetId == targetId && report.targetUser == targetUser && report.userId == userId){
+                        fail.error = error.invalReq
+                        fail.errdesc = '이미 신고하신 댓글입니다.'
+                        this.res.status(400).send(fail)
+                        return;
+                    }
+                }
+                console.log('신고 등록 시작')
+                await this.Dynamodb.put(insertParams).promise()
+                console.log('신고 등록 성공')
+                success.data = '신고 등록 성공'
+                this.res.status(201).send(success)
+            }
+            catch(err){
+                fail.error = error.dbError
+                fail.errdesc = err
+                this.res.status(521).send(fail)
+            }
+        }
+        run()
+    }
+    public read(params: any, ReadType?: toRead): void {
+        throw new Error("Method not implemented.");
+    }
+    public update(params: any): void {
+        throw new Error("Method not implemented.");
+    }
+    public delete(params: any): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
